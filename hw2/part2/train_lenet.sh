a7a88fcc0923a4e7fd3887bfb51e38ab641aed27
aimichelle <aimichelle@berkeley.edu>
2015-03-08 21:25:51 -0700

    Writeup for p2, still unsure on some parts

---
+++ hw2/part2/train_lenet.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+time ./build/tools/caffe train --solver=examples/mnist/lenet_solver.prototxt
---
+++ hw2/part2/train_mnistFC.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+time ./build/tools/caffe train --solver=examples/mnist/mnistFC_solver.prototxt
