0fe1d1e24537092f3fd8ec41ac5617cc125135e5
aimichelle <aimichelle@berkeley.edu>
2015-03-27 14:15:17 -0700

    F was wrong size, error still large

--- Assignment 3/starter_code/code/find_rotation_translation.m
+++ Assignment 3/starter_code/code/find_rotation_translation.m
@@ -1,6 +1,6 @@
 function [R, t] = find_rotation_translation(E)
-
-
+    [U,S,V] = svd(E);
+    
 
 
 end
--- Assignment 3/starter_code/code/fundamental_matrix.m
+++ Assignment 3/starter_code/code/fundamental_matrix.m
@@ -25,11 +25,14 @@ function [F, res_err] = fundamental_matrix(matches)
     F = U2 * [S2(1,:);S2(2,:);0,0,0] * V2;
     
     % Denormalization
-    F = t2'*F*t1;
+    t1 = [p1_scale,0,0; 0,p1_scale,0; -p1_scale*mean(matches(:,1:2)),1];
+    t2 = [p2_scale,0,0; 0,p2_scale,0; -p2_scale*mean(matches(:,1:2)),1];
+    F = t2*F*t1';
     
     % Find residual
-    epi1 = matches(:,1:2)*F;
-    epi2 = matches(:,3:4)*F;
-    res_err = mean([(epi1(:,1) - matches(:,1:1)).^2 + (epi1(:,2) - matches(:,2:2)).^2 ; (epi2(:,1) - matches(:,3:3)).^2 + (epi2(:,2) - matches(:,4:4)).^2])
+    epi1 = transpose(F*[matches(:,1:2) ones(size(matches, 1), 1)]');
+    epi2 = transpose(F*[matches(:,3:4) ones(size(matches, 1), 1)]');
+    %epi2 = matches(:,3:4)*F;
+    res_err = mean([(epi1(:,1) - matches(:,1:1)).^2 + (epi1(:,2) - matches(:,2:2)).^2 +  (epi1(:,3) - 1).^2; (epi2(:,1) - matches(:,3:3)).^2 + (epi2(:,2) - matches(:,4:4)).^2 + (epi2(:,3) - 1).^2]);
     
 end
